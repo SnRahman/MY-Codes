9a2a8bf02ff90310df3c052695bf690fd9aa60d9
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-02 23:54:50 +0500

    manually multiplying the numbers upto 32 digits. issue of while loop at addition is resolved.

--- manualmultiplication.py
+++ manualmultiplication.py
@@ -1,19 +1,21 @@
 
-a = 12345
-b = 921
+a = 12345678901234567890
+b = 12345678901234567890
 
 digits_of_a = []
 digits_of_b = []
 rows = []
 
+#parsing digists from variable to list
 def parsing_digits(value,list_to_store ):
     while value % 10 != 0  or value !=0:
         digit = value % 10
         remaining = int(value/10)
         list_to_store.append(digit)
-        #print('Value {} and remaing {}'.format(digit ,remaining))
         value = remaining
 
+#after 16 digits from decimal point, values goes in exponent power. to avoid such thing divide the
+#value before 16 digits
 def check_value(i,value):
     length = len(str(i))
     if length >= 16:
@@ -24,13 +26,17 @@ def check_value(i,value):
     else:
         parsing_digits(i,value)
 
+#Reverse the list
 def Reverse(lst):
     lst.reverse()
     return lst
 
+#call the functions to store the values from variable to list
 check_value(a,digits_of_a)
 check_value(b,digits_of_b)
 
+
+#Multiplication 
 count = 0
 for i in digits_of_b:   
     carry = 0
@@ -58,24 +64,25 @@ for i in digits_of_b:
     rows[count] = Reverse(rows[count])
     count+=1
 
-
-print(digits_of_a)
-print(digits_of_b)
-
-
-for row in rows:
-    print(row)
-
 final_result = []
 addition = base = carry = 0
 
-while len(rows[0] )> 0 or len(rows[1] )> 0 or len(rows[2] )> 0:
+
+#Performaing the final addition
+while len(rows) > 0:
+    if rows[0] == []:
+        rows.remove(rows[0])
+
     for i in range(0,len(rows)):
-        if len(rows[i] ) == 0:
+        if len(rows[i] ) == 0 :
+            print('row is : {}'.format(rows[i]))
+            print('index is : {}'.format(i))    
             pass
         else: 
             addition += rows[i].pop()
     addition += carry
+    print('addition is {}'.format(addition))
+    print(rows)
     carry = 0
     if addition > 9:
         base = addition % 10 
@@ -86,7 +93,17 @@ while len(rows[0] )> 0 or len(rows[1] )> 0 or len(rows[2] )> 0:
         carry = 0
     addition = 0
 
-print('Final Result is: {}'.format(Reverse(final_result)))
+reverse_list = Reverse(final_result)
+
+#Removing the extra Zeros
+while reverse_list[0] == 0:
+    print('call')
+    reverse_list.remove(reverse_list[0])
+
+
+
+print('Final Result is: {}'.format(reverse_list))
+print(a*b)
 
 
 
