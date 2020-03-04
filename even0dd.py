0340f1c7d6cd8e5641050f566cf8ae43108c6866
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-04 23:18:54 +0500

    calculate the even and Odd numbers.

---
+++ even0dd.py
@@ -0,0 +1,10 @@
+print('*' * 30)
+print('Check Number is Even or odd')
+print('*' * 30)
+
+var = int(input("Enter the Number : "))
+
+if (var % 2 == 0):
+  print("Number is even ")
+else:
+  print("Number is Odd ")
