0049724f52bda2f740dd691a5c4855b0129859bc
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-04 23:16:37 +0500

    perform subtraction, multiplications and division on fractional values.

--- lcmprogram.py
+++ lcmprogram.py
@@ -1,18 +1,31 @@
-# Calculating LCM
+print('*' * 40)
+print('Performing calculatons on Fractions')
+print('*' * 40)
 
-print('Enter Numerator of 1st value ')
-first_numerator = int(input())
-print('Enter Denominator of 1st value ')
-first_denominator = int(input())
-print('Enter Numerator of 2nd value ')
-second_numerator = int(input())
-print('Enter Denominator of 2nd value')
-second_denominator = int(input())
+first_numerator = int(input('Enter Numerator of 1st value : '))
+first_denominator = int(input('Enter Denominator of 1st value : '))
+second_numerator = int(input('Enter Numerator of 2nd value : '))
+second_denominator = int(input('Enter Denominator of 2nd value : '))
+operator = input('Enter the Operator : ')
 
-cal_num_one = cal_num_two = cal_deno = 0    
+cal_num_one = cal_num_two = cal_deno = cal_nume = 0    
 
-cal_deno = first_denominator * second_denominator 
-cal_num_one = cal_deno / first_denominator * first_numerator
-cal_num_two = cal_deno / second_denominator * second_numerator
-answer = (cal_num_one + cal_num_two) /cal_deno
-print('Answer is {}'.format(answer))
+if operator == '+': 
+    cal_deno = first_denominator * second_denominator 
+    cal_num_one = cal_deno / first_denominator * first_numerator
+    cal_num_two = cal_deno / second_denominator * second_numerator
+    cal_nume = round(cal_num_one + cal_num_two,)
+elif operator == '-':
+    cal_deno = first_denominator * second_denominator 
+    cal_num_one = cal_deno / first_denominator * first_numerator
+    cal_num_two = cal_deno / second_denominator * second_numerator
+    cal_nume = round(cal_num_one - cal_num_two,)
+elif operator == '*':
+    cal_nume = first_numerator * second_numerator
+    cal_deno = first_denominator * second_denominator
+elif operator == '/':
+    cal_nume = first_numerator * second_denominator
+    cal_deno = first_denominator * second_numerator
+else:
+    print('Wrong Operator')
+print('Answer is {} / {}'.format(cal_nume,cal_deno))
