568803ec2157c50186e19a3581199cce5d94002c
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-04 23:19:58 +0500

    check the given number is the multiples of 89 or not.

---
+++ multipleof89.py
@@ -0,0 +1,10 @@
+print('*' * 40)
+print('Check Number is Multiple of 89 or not')
+print('*' * 40)
+
+var = int(input("Enter the Number : "))
+
+if (var % 89 == 0):
+  print("Number is Multiple of 89 ")
+else:
+  print("Number is Not Multiple of 89")
