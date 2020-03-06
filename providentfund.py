2f8f9a9d175ecc9a2ed4db65ba68174e08c967aa
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-07 00:41:28 +0500

    calculate the employees provident fund at 8%/Monthly.

---
+++ providentfund.py
@@ -0,0 +1,10 @@
+print('#' * 40)
+print('Calculate the provident fund')
+print('#' * 40)
+
+base_salary = 35000
+pf = 35000 * 0.08
+employed_duration = 39
+total_pf = pf * employed_duration
+
+print('Provident Fund of 30 Months is : {}'.format(total_pf))
