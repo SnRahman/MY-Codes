ef2a60692ac5ef27496e05cb0a4de45884f39ecf
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-06 21:59:11 +0500

    print line of stars in one line using for loop.

---
+++ lineofstars.py
@@ -0,0 +1,11 @@
+print('#' * 40)
+print('Line of stars using loop')
+print('#' * 40)
+
+limit = 10
+symbol = '*'
+limit = int(input('Enter the limit : '))
+symbol = input('Enter the limit : ')
+
+for i in range(limit):
+	print(symbol, end ='')
