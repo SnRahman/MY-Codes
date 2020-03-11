f2e2437dcdb5df284d86c7bb7cb484b652c972ff
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-12 00:08:27 +0500

    find the bill of 5 month including late payment surchages using for loop.

--- latebillpayment.py
+++ latebillpayment.py
@@ -3,9 +3,13 @@ print('Bill Late payment')
 print('#' * 40)
 
 starting_bill = 6565
-current_bill = 6565 * 5
-surcharger = current_bill * 0.12
-current_bill = current_bill - surcharger
+current_bill = 0
+surcharger = 0
 
-print('current bill is: {}'.format(current_bill))
+for i in range(1,6):
+    current_bill += starting_bill 
+    surcharger = current_bill * 0.12
+    current_bill += surcharger
+    #print('Bill for month {} is: {}'.format(i,current_bill))
 
+print('current bill is: {}'.format(round(current_bill,)))
