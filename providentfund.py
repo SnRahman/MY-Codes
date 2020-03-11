b11ac3b12d311171755f715b438f208ac7b83c71
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-11 23:55:39 +0500

    calculate the profident fund of employee also the interest paid on that fund.

--- providentfund.py
+++ providentfund.py
@@ -5,6 +5,14 @@ print('#' * 40)
 base_salary = 35000
 pf = 35000 * 0.08
 employed_duration = 39
-total_pf = pf * employed_duration
+total_pf = 0
+interest = total_interest = 0
 
-print('Provident Fund of 30 Months is : {}'.format(total_pf))
+for i in range(1,40):
+    total_pf += pf*2
+    interest = total_pf * 0.06
+    total_interest += interest
+    total_pf += interest
+    #print('Provident Fund of Months {} is : {} interest is : {}'.format(i,round(total_pf,),round(interest,)))
+
+print('Provident Fund of 39 Months is : {} Total Interest is : {}'.format(round(total_pf,),round(total_interest,)))
