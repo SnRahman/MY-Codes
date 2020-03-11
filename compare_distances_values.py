516afe15c26ee2a4337d8229875c23eaa4e84db2
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-12 01:25:01 +0500

    take feel and inches in single input as float.first seperate the feets and inches and then check which value is greater using if else and fuctions.

--- compare_distances_values.py
+++ compare_distances_values.py
@@ -1,18 +1,40 @@
-first_value_feet = int(input('1st value Feet(s) : '))
-first_value_inches = int(input('1st value Inche(s) : '))
 
-second_value_feet = int(input('2nd value Feet(s) : '))
-second_value_inches = int(input('2nd value Inche(s) : '))
 
-if first_value_feet > second_value_feet:
-    print('Greater Value is : {} Feets {} Inches'.format(first_value_feet,first_value_inches))
-elif first_value_feet == second_value_feet:
-    if first_value_inches > second_value_inches:
-        print('Greater Value is : {} Feets {} Inches'.format(first_value_feet,first_value_inches))
-    elif first_value_feet == second_value_feet:
-        print("Both Values are same : {} Feets {} Inches and {} Feets {} Inches"
-            .format(first_value_feet,first_value_inches,second_value_feet,second_value_inches))
+def cal_int_fractional(num):
+    length_of_num = len(str(num))
+    int_num = round(num // 1,)
+    lenght_of_int = len(str(int_num))
+    decimal_num = round(num % 1,length_of_num-lenght_of_int)
+
+    return int_num , decimal_num
+
+
+def calculation(f1,i1,f2,i2):
+    if f1 > f2:
+        print('Greater Value is : {} Feets {} Inches'.format(f1,i1))
+    elif f1 == f2:
+        if i1 > i2:
+            print('Greater Value is : {} Feets {} Inches'.format(f1,i1))
+        elif f1 == f2:
+            print("Both Values are same : {} Feets {} Inches and {} Feets {} Inches"
+                .format(f1,i1,f2,i2))
+        else:
+            print('Greater Value is : {} Feets {} Inches'.format(f2,i2))
     else:
-        print('Greater Value is : {} Feets {} Inches'.format(second_value_feet,second_value_inches))
-else:
-    print('Greater Value is : {} Feets {} Inches'.format(second_value_feet,second_value_inches))
+        print('Greater Value is : {} Feets {} Inches'.format(f2,i2))
+
+def main():
+    first_value = input('1st value Feet(s) and Inches : ')
+    first_value_cal = cal_int_fractional(float(first_value))
+    first_value_feet = first_value_cal[0]
+    first_value_inches = first_value_cal[1]
+
+    second_value = input('1st value Feet(s) and Inches : ')
+    second_value_cal = cal_int_fractional(float(second_value))
+    second_value_feet = second_value_cal[0]
+    second_value_inches = second_value_cal[1]
+
+    calculation(first_value_feet,first_value_inches,second_value_feet,second_value_inches)
+
+if __name__ == "__main__":
+    main()
