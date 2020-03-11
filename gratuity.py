248b5bfc9e734b24805dca8a76063e9fad9f8377
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-03-11 23:23:33 +0500

    update the code of finding gratuity, calculate the current salary, annual duraion and then gratuity.

--- gratuity.py
+++ gratuity.py
@@ -3,14 +3,13 @@ print('Calculate the Gratuity')
 print('#' * 40)
 
 duration = 43
-start_salary = 36000
+start_salary = current_salary= 36000
 anual_increment = 15000
-gratuity = 0
 
 for i in range(1,duration+1):
-    if i % 13 == 0:
-        start_salary += anual_increment
-    gratuity += start_salary
-print('Months : {} Current salary : {} Gratuity : {}'.format(i,start_salary,gratuity))
-    
+    if i % 12 == 0:
+        current_salary += anual_increment
 
+annual_duration = duration/12
+gratuity = annual_duration * current_salary    
+print('Months : {} Current salary : {} Gratuity : {}'.format(i,current_salary,gratuity))
