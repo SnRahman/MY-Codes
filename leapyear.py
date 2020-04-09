b018c499a004dbf9d2abe331c47bfad0bda21111
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-04-09 20:04:37 +0500

    add another function of checking leap year by step by step if conditions and also check whiter the given year is in Gregorion celender or not.

--- leapyear.py
+++ leapyear.py
@@ -9,3 +9,18 @@ elif year % 400 == 0:
     print('is a leap year')
 else:
     print('not a leap year')
+
+#2nd mathod
+year = int(input("Enter a year: "))
+
+if year >=1882:
+    if year%4 != 0:
+        print('Common year')
+    elif year%100 != 0:
+        print('Leap year')
+    elif year%400 !=0:
+        print('Common year')
+    else:
+        print('Leap year')
+else:
+    print('Not within the Gregorian calendar period')
