ecbbdbdf6abef48350b278f658f39496fb0a19ed
Shahzada Nouman <shahzadanouman@hotmail.com>
2020-04-09 20:15:26 +0500

    form a fibonacci series by recursive function which will be formed by adding its previous two numbers.

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+def fib(n):
+    if n < 1:
+         return None
+    if n < 3:
+        return 1
+
+    elem1 = elem2 = 1
+    sum = 0
+    for i in range(3, n + 1):
+        sum = elem1 + elem2
+        elem1, elem2 = elem2, sum
+    return sum
+
+#Recursive function
+def fib_recursive(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+    return fib_recursive(n - 1) + fib_recursive(n - 2)
+
+
+for n in range(1, 11): # testing
+    print(n, "->", fib(n))
